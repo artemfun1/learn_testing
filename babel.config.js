22cf3948ed7e6a05d86ae623cdc0725ee2036ffa
Artem Peshkov <a.o.peshkov@yandex.ru>
2024-04-07 19:21:02 +0500

    testing

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+export const presets = [
+  ['@babel/preset-env', { targets: { node: 'current' } }],
+  ['@babel/preset-react', { runtime: 'automatic' }],
+  '@babel/preset-typescript',
+]
+
+
+// export const presets = [
+//   '@babel/preset-env',
+//   ['@babel/preset-react', { runtime: 'automatic' }]
+//   ,
+// ]
--- src/tests/test.spec.ts
+++ src/tests/test.spec.ts
@@ -1,7 +1,28 @@
+// import {describe, expect, test} from '@jest/globals';
 
-// import { CardWithText } from '../components/CardWithText'
+function sum(a: number, b: number) {
+	return a + b;
+}
 
-it('should be valid',()=>{
-  expect(1).toBe(1)  
-})
+describe("learn_jest", () => {
+	test("should be valid", () => {
+		expect(1).toBe(1);
+	});
 
+	test("adds 1 + 2 to equal 3", () => {
+		expect(sum(1, 2)).toBe(3);
+	});
+	test("object assignment", () => {
+		const data: {
+			[name: string]: unknown;
+		} = { one: 1 };
+		data["two"] = 2;
+		expect(data).toEqual({ one: 1, two: 2 });
+	});
+	test("adds 1 + 2 to NOT to be 5", () => {
+		expect(sum(1, 2)).not.toBe(5);
+	});
+
+
+
+});
